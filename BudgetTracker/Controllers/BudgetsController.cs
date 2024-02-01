0a6cf7de3d720973c10fc89ec88e63c1386f5faa
kuoraite <kuoraite.simona@gmail.com>
2024-02-01 17:56:47 +0200

    Created budget controller and index view

---
+++ BudgetTracker/Controllers/BudgetsController.cs
@@ -0,0 +1,95 @@
+﻿using BudgetTracker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BudgetTracker.Controllers
+{
+    public class BudgetsController : Controller
+    {
+        public readonly BudgetTrackerDbContext context;
+
+        public BudgetsController(BudgetTrackerDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: BudgetsController
+        public ActionResult Index()
+        {
+            var budgets = context.Budgets.ToList();
+            return View(budgets);
+        }
+
+        // GET: BudgetsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: BudgetsController/Create
+        public ActionResult Create()
+        {
+            ViewBag.Action = "create";
+
+            return View();
+        }
+
+        // POST: BudgetsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Budget budget)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Budgets.Add(budget);
+                context.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(budget);
+        }
+
+        // GET: BudgetsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: BudgetsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: BudgetsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: BudgetsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
