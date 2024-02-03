b5291715ded7b142231c17a43d5b8db8e393da4b
kuoraite <kuoraite.simona@gmail.com>
2024-02-03 19:39:10 +0200

    Developed the budget details view and incorporated the functionality to add income

---
+++ BudgetTracker/Controllers/IncomesController.cs
@@ -0,0 +1,31 @@
+﻿using BudgetTracker.Models;
+using BudgetTracker.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetTracker.Controllers
+{
+    public class IncomesController : Controller
+    {
+        public readonly BudgetTrackerDbContext context;
+
+        public IncomesController(BudgetTrackerDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateIncome(BudgetDetailsViewModel viewModel)
+        {
+            Income newIncome = viewModel.NewIncome;
+
+            context.Incomes.Add(newIncome);
+            context.SaveChanges();
+
+            return RedirectToAction("details", "budgets", new { id = viewModel.NewIncome.BudgetId });
+        }
+    }
+}
