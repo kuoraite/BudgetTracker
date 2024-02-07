cab7bba79340dcab36fc139cf768b1550d9be8a3
kuoraite <kuoraite.simona@gmail.com>
2024-02-07 21:36:06 +0200

    updated the layout of the tables in budget details view

--- BudgetTracker/obj/Debug/net8.0/BudgetTracker.AssemblyInfo.cs
+++ BudgetTracker/obj/Debug/net8.0/BudgetTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3898b2d831df72cc415c5fe103b1a578d9cfa89a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a139297e4bb3571915e8b74e6c68b91a0a9073bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
